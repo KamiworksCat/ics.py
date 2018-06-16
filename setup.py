91e224b3cdac577544748bf092f3d5dc1a671dab
Wong Wai Keat <waikeat@meta.us>
2018-06-16 18:09:37 +0800

    Increase python version to 3.6 due to f string usage

--- setup.py
+++ setup.py
@@ -49,9 +49,6 @@ setup(
         'Topic :: Office/Business :: Scheduling',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
 
     ],
